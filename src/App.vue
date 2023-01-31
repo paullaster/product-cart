1325fbdf2048fd8df1e55564401958047eec297a
paullaster <paullasterokoth98@gmail.com>
2023-01-31 05:58:52 +0300

    add files changes

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,3 @@
       </header>
   <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
