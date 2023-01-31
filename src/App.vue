1b4f0f47d3712ee823b714af6ad51a35f5a1c7b8
paullaster <paullasterokoth98@gmail.com>
2023-01-31 04:54:57 +0300

    add files changes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/product">Products</router-link>
+    <router-link to="/past-orders">Past orders</router-link>
   </div>
 </template>
 
