42ceae21c72f67f0fffcf7a3429848e71d80c1c4
paullaster <paullasterokoth98@gmail.com>
2023-01-31 04:56:13 +0300

    add files changes

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,6 @@
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
-    
-  }
-}
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
