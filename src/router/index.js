406b0b975d23c16ea6d84eab937e6ca1cdb07a6d
paullaster <paullasterokoth98@gmail.com>
2023-01-31 05:14:04 +0300

    add files changes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes =[
         path: '/',
         name: 'Home',
         component: HomePage
+    },
+    {
+        path: '/products',
+        name: 'Products',
+        component: ProductsPage
     }
 ]
 
