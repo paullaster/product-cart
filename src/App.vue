327f4533b9df8c6a72318019cb119bdf2f60b08c
paullaster <paullasterokoth98@gmail.com>
2023-01-31 04:52:59 +0300

    add files changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+  </div>
 </template>
 
 <script>
