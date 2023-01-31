158f3ee859da7d6a4faca1ec04848b5bccaa41d0
paullaster <paullasterokoth98@gmail.com>
2023-01-31 06:08:44 +0300

    add files changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,55 @@
 <template>
-    <div>
-        Hi from Home
+    <div class="splash-container">
+        <div class="splash">
+            <h1>Splendid Food</h1>
+        </div>
     </div>
+
+    <main class="wrapper">
+        <h2>Recommended</h2>
+
+        <div class="recommended">
+            <div v-for="(product, index) in inventory.slice(0, 3)" :key="product.id" class="card">
+                <div class="card-title">{{ product.name }}</div>
+                <div class="card-body">
+                    <i :class="productIcon(product.icon)"></i>
+                    <form>
+                        <div class="row">
+                            <div class="cell">
+                                <label>Type:</label>
+                            </div>
+                            <div class="cell">
+                                <em>{{ product.type }}</em>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="cell">
+                                <label>Price:</label>
+                            </div>
+                            <div class="cell">${{ product.price.USD }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="cell">
+                                <label>Quantity:</label>
+                            </div>
+                            <div class="cell">
+                                <input type="number" v-model.number="product.quantity" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <button @click="addToCart(product.name, index)" class="btn btn-light">
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
- name: "HomePage",
+    name: "HomePage",
 }
 </script>
