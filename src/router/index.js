47d449b9e521036ecbd929e1463ee391b48063c7
paullaster <paullasterokoth98@gmail.com>
2023-01-31 05:18:34 +0300

    add files changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage';
 import ProductsPage from '../views/ProductsPage';
+import PastOrdersPage from '../views/PastOrdersPage';
 
 const routes =[
     {
