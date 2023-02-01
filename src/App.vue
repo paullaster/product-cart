f0066ae05e156d5f8ec750b6d214195ac928682b
paullaster <paullasterokoth98@gmail.com>
2023-02-01 05:08:43 +0300

    commit changes

--- src/App.vue
+++ src/App.vue
@@ -3,83 +3,77 @@ import SidebarComponent from './components/Sidebar-component.vue';
 
 <template>
   <header class="top-bar spread">
-        <nav class="top-bar-nav">
-          <router-link to="/" class="top-bar-link">
-            <i class="icofont-spoon-and-fork"></i>
-            <span>Home</span>
-          </router-link>
-          <router-link to="/products" class="top-bar-link">
-            <span>Products</span>
-          </router-link>
-          <router-link to="/past-orders" class="top-bar-link">
-            <span>Past Orders</span>
-          </router-link>
-        </nav>
-        <div class="top-bar-cart-link">
-          <i class="icofont-cart-alt icofont-1x"></i>
-          <span @click="toggleCart">Cart({{itemsInCart}})</span>
-        </div>
-      </header>
-  <router-view :inventory="inventory"/>
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span @click="toggleCart">Cart({{ itemsInCart }})</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" />
 
-  <SidebarComponent
-    v-if="showSidebar" 
-    :toggle="toggleCart" 
-    :cart="cart"
-    :inventory="inventory" 
-    :icon="productIcon" 
-    :remove="removeItem"
-  />
+  <SidebarComponent v-if="showSidebar" :toggle="toggleCart" :cart="cart" :inventory="inventory" :icon="productIcon"
+    :remove="removeItem" />
 </template>
 
 <script>
-  import SidebarComponent from './components/SidebarComponent.vue';
-  import food from './food.json';
+import SidebarComponent from './components/SidebarComponent.vue';
+import food from './food.json';
 
-  export default {
+export default {
   methods: {
     productIcon(icon) {
-            return `icofont-10x icofont-${icon}`;
-          },
-          addToCart(name, itemID) {
-            if (!this.cart[name]) this.cart[name] = 0;
-            this.cart[name] += this.inventory[itemID].quantity;
-            this.inventory[itemID].quantity  = 0;
-            console.log(this.cart);
-          },
-          toggleCart() {
-            this.showSidebar = !this.showSidebar;
-          },
-          randomIndex() {
-            const startIndex = Math.floor(
-              Math.random() * (this.inventory.length / 2)
-            );
-            const endIndex = Math.floor(Math.random() * this.inventory.length);
-            return [startIndex, endIndex];
-          },
-          removeItem(key, object){
-            delete object[key];
-          },
+      return `icofont-10x icofont-${icon}`;
+    },
+    addToCart(name, itemID) {
+      if (!this.cart[name]) this.cart[name] = 0;
+      this.cart[name] += this.inventory[itemID].quantity;
+      this.inventory[itemID].quantity = 0;
+      console.log(this.cart);
+    },
+    toggleCart() {
+      this.showSidebar = !this.showSidebar;
+    },
+    randomIndex() {
+      const startIndex = Math.floor(
+        Math.random() * (this.inventory.length / 2)
+      );
+      const endIndex = Math.floor(Math.random() * this.inventory.length);
+      return [startIndex, endIndex];
+    },
+    removeItem(key, object) {
+      delete object[key];
+    },
   },
-  computed:{
-    itemsInCart(){
-            const items = Object.values(this.cart).reduce(
-              (acc, curr) => {
-                return acc + curr;
-              }, 0
-            );
-            return items;
-          },
+  computed: {
+    itemsInCart() {
+      const items = Object.values(this.cart).reduce(
+        (acc, curr) => {
+          return acc + curr;
+        }, 0
+      );
+      return items;
+    },
   },
-  data () {
+  data() {
     return {
       showSidebar: true,
       inventory: food,
-      cart:{}
-    }  
+      cart: {}
+    }
   }
-,    components: {
-      SidebarComponent
-    },
-  }
+  , components: {
+    SidebarComponent
+  },
+}
 </script>
