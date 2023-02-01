241e722f3d52d0ae1563180ae9947db64543fa09
paullaster <paullasterokoth98@gmail.com>
2023-02-01 06:05:38 +0300

    commit changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,6 +14,7 @@
             :addToCart="addToCart"
             :icon="icon"
             :index="index"
+            :product="product"
             :key="product.id"
             />
         </div>
