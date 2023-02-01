fd8b1f24505a8fb898178034f8dcc5ce6a368e9a
paullaster <paullasterokoth98@gmail.com>
2023-02-01 06:12:02 +0300

    commit changes

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -3,13 +3,17 @@
         <h1>Products</h1>
 
         <div class="card-container">
-        
+          <ProductCard
         </div>
       </main>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard.vue';
+
+
 export default {
     name: "ProductPage",
+    components: { ProductCard }
 }
 </script>
